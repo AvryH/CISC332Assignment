a6275a9078353a4e8e66544a0d4688a5bf35019c
Ben-Alderson <bennyalderson@gmail.com>
2018-03-22 16:05:28 -0400

    Add basic template for signin, signup, and movies.

---
+++ movie.php
@@ -0,0 +1,69 @@
+<?PHP
+	//////////
+	// Valid types of request to this page:
+	// POST action="addReview" title=? customerReview=? customerRating=? | Adds a review into the `watched` table for this customer
+	// GET title=? | Returns the page for the movie with the given title
+	//////////
+
+	// Connect to the server
+	require_once(__DIR__ . "/connect.php");
+
+	if($_SERVER["REQUEST_METHOD"] === "POST") {
+		if($_POST["action"] === "addReview") {
+			$query = $db->prepare("INSERT INTO `watched`(acctNum, title, customerRating, customerReview) VALUES (?, ?, ?, ?)");
+			$query->execute([$_SESSION["acctNumber"], $_POST["title"], $_POST["customerRating"], $_POST["customerReview"]]);
+		}
+	}
+
+	// Load the movie from the database
+	$query = $db->prepare("SELECT * FROM `movie` WHERE title=?");
+	$query->execute([$_REQUEST["title"]]);
+	$movie = $query->fetch(PDO::FETCH_ASSOC);
+
+	// Load the list of presentations from the database
+	$query = $db->prepare("SELECT complexName, theaterNum, startTime FROM `presentation` JOIN `movie` ON title=movieTitle NATURAL JOIN `showing` WHERE title=?");
+	$query->execute([$_REQUEST["title"]]);
+	$presentations = $query->fetchAll(PDO::FETCH_ASSOC);
+
+	// Load all reviews
+	$query = $db->prepare("SELECT customerRating, customerReview FROM `watched` WHERE title=?");
+	$query->execute([$_REQUEST["title"]]);
+	$reviews = $query->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+Movie:<br>
+<?PHP
+	print_r($movie);
+?>
+
+<br><br>Presentations:<br>
+<?PHP
+	foreach($presentations as $pres) {
+		print_r($pres);
+		echo("<br>");
+	}
+?>
+
+<?PHP
+	// Only show the "add review" section to users who are logged in
+	if(isset($_SESSION["acctNumber"])) {
+?>
+<br><br>Add review:<br>
+<form method="POST">
+	<input name="action" type="hidden" value="addReview"></input>
+	<input name="title" type="hidden" value="<?PHP echo($_REQUEST["title"]); ?>"></input>
+	<textarea name="customerReview" placeholder="Review text"></textarea>
+	<input name="customerRating" type="number" placeholder="Rating from 1-5 stars"></input>
+	<input type="submit" value="Submit review"></input>
+</form>
+<?PHP
+	}
+?>
+
+<br><br>Reviews:<br>
+<?PHP
+	foreach($reviews as $rev) {
+		print_r($rev);
+		echo("<br>");
+	}
+?>
---
+++ signIn.php
@@ -0,0 +1,106 @@
+<?PHP
+	//////////
+	// Valid types of request to this page:
+	// POST action="login", acctNumber=?, password=? | Logs the user on
+	// POST action="logout" | Logs the user off
+	// POST action="updateUser" | Updates user info
+	// GET | Returns this page
+	//////////
+	
+	require_once(__DIR__ . "/connect.php");
+
+	if($_SERVER["REQUEST_METHOD"] === "POST") {
+		if($_POST["action"] === "login") {
+			// Load the hashed password from the database
+			$acctNumber = $_POST["acctNumber"];
+			$stmt = $db->prepare("SELECT password FROM `customer` WHERE acctNum=?");
+			$stmt->execute([$acctNumber]);
+			$hash = $stmt->fetchColumn();
+
+			if($hash !== false) {
+				if(password_verify($_POST["password"], $hash)) {
+					// The password was correct
+					$_SESSION["acctNumber"] = $acctNumber;
+				} else {
+					echo("The password was incorrect");
+					exit();
+				}
+			} else {
+				echo("No user with that id was found");
+				exit();
+			}
+		} else if($_POST["action"] === "logout") {
+			unset($_SESSION["acctNumber"]);
+			// The user is no longer logged in
+		} else if($_POST["action"] === "updateUser") {
+			$query = $db->prepare("UPDATE `customer` SET fName=:fName, lName=:lName, phoneNum=:phoneNum, street=:street, city=:city, pc=:pc, email=:email, CCNum=:CCNum, CCExp=:CCExp, password=IF(ISNULL(:password),password,:password) WHERE acctNum=:acctNum;");
+
+			$query->bindValue(":acctNum", $_SESSION["acctNumber"]);
+			$query->bindValue(":fName", $_POST["fName"]);
+			$query->bindValue(":lName", $_POST["lName"]);
+			$query->bindValue(":phoneNum", $_POST["phoneNum"]);
+			$query->bindValue(":street", $_POST["street"]);
+			$query->bindValue(":city", $_POST["city"]);
+			$query->bindValue(":pc", $_POST["pc"]);
+			$query->bindValue(":email", $_POST["email"]);
+			$query->bindValue(":CCNum", $_POST["CCNum"]);
+			$query->bindValue(":CCExp", $_POST["CCExp"]);
+			if(isset($_POST["password"]) && $_POST["password"] !== "") {
+				$query->bindValue(":password", password_hash($_POST["password"], PASSWORD_DEFAULT));
+			} else {
+				$query->bindValue(":password", null);
+			}
+
+			$query->execute();
+		}
+	}
+?>
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+	</head>
+	<body>
+<?PHP
+	if(isset($_SESSION["acctNumber"])) {
+			$query = $db->prepare("SELECT * FROM `customer` WHERE acctNum=:acctNum;");
+			$query->bindValue(":acctNum", $_SESSION["acctNumber"]);
+			$query->execute();
+			$user = $query->fetch(PDO::FETCH_ASSOC);
+?>
+		<form method="POST">
+			<input name="action" type="hidden" value="logout"></input>
+			<input type="submit" value="Logout"></input>
+		</form>
+
+		<form method="POST">
+			<input name="action" type="hidden" value="updateUser"></input>
+			<input name="fName" type="text" placeholder="fName" value="<?PHP echo(htmlspecialchars($user["fName"])); ?>"></input><br>
+			<input name="lName" type="text" placeholder="lName" value="<?PHP echo(htmlspecialchars($user["lName"])); ?>"></input><br>
+			<input name="phoneNum" type="text" placeholder="phoneNum" value="<?PHP echo(htmlspecialchars($user["phoneNum"])); ?>"></input><br>
+			<input name="street" type="text" placeholder="street" value="<?PHP echo(htmlspecialchars($user["street"])); ?>"></input><br>
+			<input name="city" type="text" placeholder="city" value="<?PHP echo(htmlspecialchars($user["city"])); ?>"></input><br>
+			<input name="pc" type="text" placeholder="pc" value="<?PHP echo(htmlspecialchars($user["pc"])); ?>"></input><br>
+			<input name="email" type="text" placeholder="email" value="<?PHP echo(htmlspecialchars($user["email"])); ?>"></input><br>
+			<input name="CCNum" type="text" placeholder="CCNum" value="<?PHP echo(htmlspecialchars($user["CCNum"])); ?>"></input><br>
+			<input name="CCExp" type="text" placeholder="CCExp" value="<?PHP echo(htmlspecialchars($user["CCExp"])); ?>"></input><br>
+			<input name="password" type="password" placeholder="(password unchanged)"></input>
+			<input type="submit" value="Update"></input><br>
+		</form>
+
+<?PHP
+	} else {
+?>
+		<form method="POST">
+			<input name="action" type="hidden" value="login"></input>
+			<input name="acctNumber" type="text" placeholder="user id"></input>
+			<input name="password" type="password" placeholder="password"></input>
+			<input type="submit" value="Login"></input>
+		</form>
+		<a href="signUp.php">Sign Up</a>
+<?PHP
+	}
+?>
+	</body>
+</html>
