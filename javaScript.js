4fdddfa4f469dcdb0f5273e53127423332050f3d
AvryH <AvryH@users.noreply.github.com>
2018-03-23 19:21:50 -0400

    signin/up/update

--- javaScript.js
+++ javaScript.js
@@ -1,2 +1,12 @@
 // This will be the JavaScript file for our project
+// <script src=”javaScript.js”></script> This will be
+// how to link files in this file to the js
 
+var arePWtheSame = function()
+	if (document.getElementById('password').value ==
+    document.getElementById('confirm_password').value){
+        document.getElementById('submit').disabled = false;
+    } 
+	else {
+        document.getElementById('submit').disabled = true;
+    }
