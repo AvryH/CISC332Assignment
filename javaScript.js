6e26057a007ca801aeab7bb2302fa49a3a818c5f
AvryH <AvryH@users.noreply.github.com>
2018-03-23 20:33:35 -0400

    javaScriptWorks

--- javaScript.js
+++ javaScript.js
@@ -2,13 +2,16 @@
 // <script src=”javaScript.js”></script> This will be
 // how to link files in this file to the js
 
-var arePWtheSame = function()
-	if (document.getElementById('password').value ==
-    document.getElementById('confirm_password').value){
-        document.getElementById('areMatching').style.color = 'green';
+var arePWtheSame = function() {
+	if (document.getElementById('password').value == document.getElementById('confirm_password').value) {
+		document.getElementById('areMatching').style.color = 'green';
 		document.getElementById('areMatching').innerHTML = 'Passwords Match';
-    } 
+		document.getElementById('submit').disabled = false;
+		
+	}
 	else {
 		document.getElementById('areMatching').style.color = 'red';
-		document.getElementById('areMatching').innerHTML = 'Passwords Do Not Match';
-    }
+		document.getElementById('areMatching').innerHTML = 'Passwords do not match';
+		document.getElementById('submit').disabled = true;
+	}
+}
