16f589d53647d0bff3c9c65d6c66a824b330ab09
Ben-Alderson <bennyalderson@gmail.com>
2018-03-27 14:15:53 -0400

    Make movie page better looking, remove mainActors column from `movie`

--- movie.php
+++ movie.php
@@ -20,6 +20,11 @@
 	$query->execute([$_REQUEST["title"]]);
 	$movie = $query->fetch(PDO::FETCH_ASSOC);
 
+	// Load the list of actors from the database
+	$query = $db->prepare("SELECT * FROM `mainactors` WHERE MovieTitle=?");
+	$query->execute([$_REQUEST["title"]]);
+	$actors = $query->fetchAll(PDO::FETCH_ASSOC);
+
 	// Load the list of showings from the database
 	$query = $db->prepare("SELECT showingID, complexName, theaterNum, startTime FROM `showing` WHERE movieTitle=?");
 	$query->execute([$_REQUEST["title"]]);
@@ -30,14 +35,35 @@
 	$query->execute([$_REQUEST["title"]]);
 	$reviews = $query->fetchAll(PDO::FETCH_ASSOC);
 ?>
+<!DOCTYPE html>
 
-Movie:<br>
-<?PHP
-	print_r($movie);
-?>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="styling.css"/>
+	</head>
+	<body>
+		<div class="movieInfo">
+			<div class="movieImage"><img src="<?PHP echo(htmlspecialchars($movie["thumbnail"])); ?>"></img></div>
+			<div class="movieTitle"><?PHP echo(htmlspecialchars($movie["title"])); ?></div>
+			<div class="movieRating"><b>Rating</b><br><?PHP echo(htmlspecialchars($movie["rating"])); ?></div>
+			<div class="moviePlot"><?PHP echo(htmlspecialchars($movie["plotSyn"])); ?></div>
+			<div class="movieDirector"><b>Director</b><br><?PHP echo(htmlspecialchars($movie["director"])); ?></div>
+			<div class="movieMainActors"><b>Main Actors</b><br><?PHP 
+	$print = [];
+	foreach($actors as $actor) {
+		array_push($print, htmlspecialchars($actor["FName"] . ' ' . $actor["LName"]));
+	}
+	echo(implode(', ', $print));
+?></div>
+			<div class="movieProdComp"><b>Production Company</b><br><?PHP echo(htmlspecialchars($movie["prodComp"])); ?></div>
+			<div class="movieDates"><b>Starts: </b><?PHP echo(htmlspecialchars($movie["startDate"])); ?><br><b>Ends:</b><?PHP echo(htmlspecialchars($movie["endDate"])); ?></div>
+			<div class="movieGenre"><b>Genre</b><br><?PHP echo(htmlspecialchars($movie["genre"])); ?></div>
+			<div class="movieSupplier"><?PHP echo(htmlspecialchars($movie["supplier"])); ?></div>
+		</div>
 
-<br><br>Showings:<br>
-<div class="movie_showings">
+		<br><br>Showings:<br>
+		<div class="movie_showings">
 <?PHP
 	foreach($showings as $show) {
 		echo('<a href="showing.php?id=' . htmlspecialchars(urlencode($show["showingID"])) . '">');
@@ -45,20 +71,20 @@ Movie:<br>
 		echo('</a><br/>');
 	}
 ?>
-</div>
+		</div>
 
 <?PHP
 	// Only show the "add review" section to users who are logged in
 	if(isset($_SESSION["acctNumber"])) {
 ?>
-<br><br>Add review:<br>
-<form method="POST">
-	<input name="action" type="hidden" value="addReview"></input>
-	<input name="title" type="hidden" value="<?PHP echo($_REQUEST["title"]); ?>"></input>
-	<textarea name="customerReview" placeholder="Review text"></textarea>
-	<input name="customerRating" type="number" placeholder="Rating from 1-5 stars"></input>
-	<input type="submit" value="Submit review"></input>
-</form>
+		<br><br>Add review:<br>
+		<form method="POST">
+			<input name="action" type="hidden" value="addReview"></input>
+			<input name="title" type="hidden" value="<?PHP echo($_REQUEST["title"]); ?>"></input>
+			<textarea name="customerReview" placeholder="Review text"></textarea>
+			<input name="customerRating" type="number" placeholder="Rating from 1-5 stars"></input>
+			<input type="submit" value="Submit review"></input>
+		</form>
 <?PHP
 	}
 ?>
